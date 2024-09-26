cdb9cec51077ac3e273425822e610835f124fc3b
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2024-09-26 22:28:54 +0200

    Release version 0.0.0.dev14

--- src/exceptionman/reporter_exception.py
+++ src/exceptionman/reporter_exception.py
@@ -1,45 +1,19 @@
-from __future__ import annotations
+from __future__ import annotations as _annotations
 
 from typing import TYPE_CHECKING as _TYPE_CHECKING
-from functools import partial as _partial
 
 from exceptionman import _traceback
 
 if _TYPE_CHECKING:
-    from rich.console import Console
     from mdit.document import Document
 
 
 class ReporterException(Exception):
     """Base exception class with HTML reporting capabilities."""
 
-    def __init__(
-        self,
-        report: Document,
-        sphinx_config: dict | None = None,
-        sphinx_args: dict | None = None,
-        console: Console | None = None,
-    ):
-        import mdit as _mdit
-
-        # console_renderable =
-        # console = _Console()
-        # with console.capture() as capture:
-        #     console.print(console_renderable)
-        # console_text = capture.get()
-        sphinx_args = {
-            "status": None,
-            "warning": None,
-        } | (sphinx_args or {})
-        target_config = _mdit.target.sphinx()
-        target_config.renderer = _partial(
-            _mdit.render.sphinx,
-            config=sphinx_config,
-            **sphinx_args
-        )
-        report.default_output_target = target_config
-        self.report = report
+    def __init__(self, report: Document):
         super().__init__()
+        self.report = report
         if not _traceback.USER_INSTALLED:
             _traceback.install(temporary=True)
         return
